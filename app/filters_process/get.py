7d5abfb53ffc840c8c8308d66bd1d2d8241b53e6
Maghwyn <christopher.samson@edu.esiee-it.fr>
2021-11-18 18:48:15 +0100

    Added a file get images/filters

---
+++ app/filters_process/get.py
@@ -0,0 +1,42 @@
+import os
+from app.log.logger import transfer_log as log
+
+
+def images(path):
+    """
+    :param path:
+    :return:
+    """
+    images_list = os.listdir(path)
+    for image in images_list:
+        if not image.endswith('.jpeg' or '.png' or '.jpg' or '.svg'):
+            log(f'ValueError : Invalid Format => {image} was ignored.\n')
+            images_list.remove(image)
+
+    return images_list
+
+
+def filters(argument):
+    """
+    :param argument:
+    :return:
+    """
+
+    filter_argument = {}
+    if '|' in argument:
+        separated_argument = argument.split('|')
+        for arg in separated_argument:
+            if ':' in arg:
+                temp_filter_argument = arg.split(':')
+                filter_argument[str(temp_filter_argument[0])] = int(temp_filter_argument[1])
+            else:
+                filter_argument[str(arg)] = str(arg)
+    else:
+        if ':' in argument:
+            temp_filter_argument = argument.split(':')
+            filter_argument[str(temp_filter_argument[0])] = int(temp_filter_argument[1])
+        else:
+            filter_argument[str(argument)] = str(argument)
+
+    log(f"Filters extracted and waiting to be applied : {filter_argument}\n")
+    return filter_argument
