e786ddafb144002fa70f01e4aeaa922971414160
Maghwyn <christopher.samson@edu.esiee-it.fr>
2021-11-18 18:45:49 +0100

    merged the terminal.py to work on both arguments and ini config

--- app/CLI/terminal.py
+++ app/CLI/terminal.py
@@ -1,150 +1,115 @@
-import cv2
 import os
 import sys
-
+from cv2 import cv2
 from app.filters_process.pipeline import pipeline as pip
-from app.log.logger import transfer_log as log, dump_log, clear_log
+from app.log.logger import transfer_log as log, clear_log, throw_log
+from app.CLI.read_CLI import reading_cli as default_config
+import app.filters_process.get as get
 
 
 def initialisation():
     """
-
     :return:
     """
-    clear_log()
+    log('\n----------------------\n  Running the file...\n-----------------------')
 
-    path = {
+    clear_log()
+    args = sys.argv
+    content = {
         "input_dir": '',
         "output_dir": '',
+        "log_file": '',
+        "filters:": '',
     }
 
+    for i, arg in enumerate(args):
+
+        if arg == '--config-file':
+            if i + 1 >= len(args):
+                log("ERROR : IndexError : You did not provided a file.")
+                return sys.exit(0)
+
+            if args[i + 1] == 'imagefilter.log':
+                content['log_file'] = args[i + 1]
+            elif args[i + 1] == 'cli.ini':
+                content = default_config(args[i + 1])
+                return content
+
+        if arg == "-h":
+            print("\nOptions :")
+            print("-h , --help\n"
+                  "-i , --input_dir <directory>\n"
+                  "-o , --output_dir <directory>\n"
+                  "--f, --filters <-h for more help>\n")
+            return sys.exit(0)
+
+    return path_analyse(content)
+
+
+def verify_submit(cmd, has_argument, default_path):
+    if has_argument >= len(cmd):
+        log(f"ERROR : IndexError : You did not provided an {default_path} path.")
+        return sys.exit(0)
+
+    directory = cmd[has_argument]
+    if default_path == 'output':
+        return directory
+
+    if not os.path.exists(f"{directory}"):
+        log("ERROR : IndexError : The path provided does not exist.")
+        return sys.exit(0)
+    else:
+        return directory
+
+
+def path_analyse(content):
+    """
+    :param content:
+    :return:
+    """
     args = sys.argv
-    log('\n----------------------\n  Running the file...\n-----------------------')
     for i, arg in enumerate(args):
 
         if arg == "-i":
-            if i + 1 >= len(args):
-                log("ERROR : IndexError : You did not provided a path.")
-                return
-
-            input_dir = args[i + 1]
-            path["input_dir"] = input_dir
+            content["input_dir"] = verify_submit(args, i + 1, "input")
 
         if arg == "-o":
-            if i + 1 >= len(args):
-                log("ERROR : IndexError : You did not provided a path.")
-                return
+            content['output_dir'] = verify_submit(args, i + 1, "output")
 
-            output_dir = args[i + 1]
-            path["output_dir"] = output_dir
-
-        if arg == "-f":
-            if args[i + 1] == '-h':
-                print('\nList of usable filters :\n')
-                print('"grayscale", ColorToGray\n"blur:integer_odd", CleanToBlur\n"dilate:integer", CleanToDilate')
-                print('Usage example : -f "blur:5|grayscale"\n')
-                return
-
-        if arg == "-h":
-            print("\nList of commands :")
-            print("-h , --help\n-i , --input_dir <directory>\n-o , --output_dir <directory>\n--f, --filters <-h for "
-                  "more help>\n")
-
-    return path
-
-
-def get_filter(argument):
-    """
-
-    :param argument:
-    :return:
-    """
-
-    filter_argument = {}
-    if '|' in argument:
-        separated_argument = argument.split('|')
-        for arg in separated_argument:
-            if ':' in arg:
-                temp_filter_argument = arg.split(':')
-                filter_argument[str(temp_filter_argument[0])] = int(temp_filter_argument[1])
-            else:
-                filter_argument[str(arg)] = str(arg)
-    else:
-        if ':' in argument:
-            temp_filter_argument = argument.split(':')
-            filter_argument[str(temp_filter_argument[0])] = int(temp_filter_argument[1])
-        else:
-            filter_argument[str(argument)] = str(argument)
-
-    log(f"Filters extracted and waiting to be applied : {filter_argument}")
-    return filter_argument
-
-
-def get_image(path):
-    images_list = os.listdir(path)
-    for image in images_list:
-        if not image.endswith('.jpeg' or '.png' or '.jpg' or '.svg'):
-            log(f'ValueError : Invalid Format => {image} was ignored.\n')
-            images_list.remove(image)
-
-    return images_list
-
-
-def throw_log():
-    args = sys.argv
-    for i, arg in enumerate(args):
-        if arg == "--cfg-file":
-            if i + 1 >= len(args):
-                print('ERROR : IndexError : You did not provided a file.')
-                return
-
-            input_log = args[i + 1]
-            if os.path.isfile(f'app/log/{input_log}'):
-                return dump_log()
-            else:
-                print('ERROR : IndexError : The file provided does not exist.')
-
-
-def processing(path):
-    """
-
-    :param path:
-    :return:
-    """
-
-    if path is None:
-        return
-
-    args = sys.argv
-    for i, arg in enumerate(args):
         if arg == '--f':
-
             if i + 1 >= len(args):
                 log("ERROR : IndexError : You did not provided a filter.")
                 return
 
-            images_list = get_image(path['input_dir'])
-            if len(images_list) == 0:
-                return log('ERROR : IndexError : The directory returned NULL.')
+            filters = args[i + 1]
+            content['filters'] = filters
 
-            # ------------------------------------------------------------------- #
+    return content
 
-            filtered_image_directory = path['output_dir']
-            if not os.path.exists(f"{filtered_image_directory}"):
-                log(f'Creating directory {filtered_image_directory}...')
-                os.makedirs(f"{filtered_image_directory}")
 
-            filter_argument = get_filter(args[i + 1])
+def processing(path):
 
-            for image in images_list:
-                log(f"Opening image data from = {path['input_dir']}/{image}")
-                image_read = cv2.imread(f"{path['input_dir']}/{image}")
-                to_directory_filter = f"{path['output_dir']}/{image}"
+    filter_argument = get.filters(path['filters'])
+    images_list = get.images(path['input_dir'])
+    filtered_image_directory = path['output_dir']
+    log_file = path['log_file']
 
-                for filter_name, argument in filter_argument.items():
-                    image_read = pip(filter_name, argument, image_read)
+    if len(images_list) == 0:
+        return log('ERROR : IndexError : The directory returned NULL.')
 
-                cv2.imwrite(to_directory_filter, image_read)
-                log(f"Save result image to = {to_directory_filter}\n")
+    if not os.path.exists(f"{filtered_image_directory}"):
+        log(f'Creating directory {filtered_image_directory}...')
+        os.makedirs(f"{filtered_image_directory}")
 
-    return throw_log()
+    for image in images_list:
+        log(f"Opening image data from = {path['input_dir']}/{image}")
+        image_read = cv2.imread(f"{path['input_dir']}/{image}")
+        to_directory_filter = f"{path['output_dir']}/{image}"
+
+        for filter_name, argument in filter_argument.items():
+            image_read = pip(filter_name, argument, image_read)
+
+        cv2.imwrite(to_directory_filter, image_read)
+        log(f"Save result image to = {to_directory_filter}\n")
+
+    return throw_log(log_file)
